c76834f4a5a0873e384114d63cf469b18ff8f546
Adler Coelho <adler.coelho.santos@icloud.com>
2020-11-06 08:34:53 -0300

    feat: integração com o slack ao adicionar cadastro

--- base-nest/src/fornecedor/fornecedor.service.ts
+++ base-nest/src/fornecedor/fornecedor.service.ts
@@ -3,32 +3,35 @@ import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Fornecedor, FornecedorDocument } from './fornecedor.schema';
 import { FornecedorDto } from './fornecedor.dto';
+import { SlackService } from 'src/shared/services/slack.service';
 
 @Injectable()
 export class FornecedorService {
     constructor(
-        @InjectModel(Fornecedor.name) private fornecedorSchema: Model<FornecedorDocument>
+        @InjectModel(Fornecedor.name) private _fornecedorSchema: Model<FornecedorDocument>,
+        private _slackService: SlackService
     ) { }
 
     async create(fornecedorDto: FornecedorDto): Promise<Fornecedor> {
-        const schema = new this.fornecedorSchema(fornecedorDto);
+        this._slackService.postMessage(fornecedorDto);
+        const schema = new this._fornecedorSchema(fornecedorDto);
         return schema.save();
     }
 
     async findOne(filters): Promise<Fornecedor> {
-        return this.fornecedorSchema.findOne(filters).exec();
+        return this._fornecedorSchema.findOne(filters).exec();
     }
 
     async findAll(): Promise<Fornecedor[]> {
-        return this.fornecedorSchema.find().exec();
+        return this._fornecedorSchema.find().exec();
     }
 
     async update(fornecedorDto: FornecedorDto, fornecedorId): Promise<Fornecedor> {
-        return this.fornecedorSchema.updateOne({'_id': fornecedorId } , fornecedorDto).exec();
+        return this._fornecedorSchema.updateOne({'_id': fornecedorId } , fornecedorDto).exec();
     }
 
     async delete(fornecedorId: IDBDatabase): Promise<any> {
-        return this.fornecedorSchema.deleteOne({_id: fornecedorId})
+        return this._fornecedorSchema.deleteOne({_id: fornecedorId})
     }
 
 }
